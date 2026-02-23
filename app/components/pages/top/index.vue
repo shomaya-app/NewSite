a43c90f2e529183eeed0676a1dbbcb8a29ddee33
shomaya-app <mayahara.dev@gmail.com>
2026-02-24 01:27:55 +0900

    #5 titleとdescriptionを設定

--- app/components/pages/top/index.vue
+++ app/components/pages/top/index.vue
@@ -1,3 +1,16 @@
 <template>
   <ViewsTop />
 </template>
+
+<script setup lang="ts">
+useHead({
+  title: '一般社団法人 日本バングラデシュコンサルティング＆トレーディング',
+  meta: [
+    {
+      name: 'description',
+      content:
+        '一般社団法人 日本バングラデシュコンサルティング＆トレーディングの公式ウェブサイト'
+    }
+  ]
+})
+</script>
