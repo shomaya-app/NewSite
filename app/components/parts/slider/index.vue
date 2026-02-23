baa88db522ea355ef4c9c5c6b76a9899601869f6
shomaya-app <mayahara.dev@gmail.com>
2026-02-24 01:25:42 +0900

    #5 topページの作成

---
+++ app/components/parts/slider/index.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="w-full">
+    <Swiper
+      :modules="[Autoplay, Pagination, Navigation]"
+      :slides-per-view="1"
+      :loop="true"
+      :autoplay="{ delay: 8000 }"
+      :pagination="{ clickable: true }"
+      :navigation="true"
+      class="h-[70vh]"
+    >
+      <SwiperSlide v-for="(slide, index) in props.slides" :key="index">
+        <div
+          class="flex h-full w-full items-center justify-center bg-cover bg-center"
+          :style="{ backgroundImage: `url(${slide.image})` }"
+        >
+          <div class="bg-black/40 p-6 text-center text-white">
+            <h2 class="pc:text-[2.2rem] text-[1.8rem] font-bold">
+              {{ slide.title }}
+            </h2>
+            <p class="pc:text-[1.8rem] mt-4 text-lg text-[1.4rem]">
+              {{ slide.description }}
+            </p>
+          </div>
+        </div>
+      </SwiperSlide>
+    </Swiper>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { Swiper, SwiperSlide } from 'swiper/vue'
+import { Autoplay, Pagination, Navigation } from 'swiper/modules'
+
+import 'swiper/css'
+import 'swiper/css/pagination'
+import 'swiper/css/navigation'
+
+type Slide = {
+  image: string
+  title: string
+  description: string
+}
+
+type Props = {
+  slides: Slide[]
+}
+
+const props = defineProps<Props>()
+</script>
+
+<style>
+/* アクティブドット */
+.swiper-pagination-bullet-active {
+  background-color: #bf842b; /* ゴールド */
+}
+
+.swiper-button-next,
+.swiper-button-prev {
+  color: #bf842b; /* ゴールド */
+}
+
+@media (max-width: 768px) {
+  .swiper-button-next,
+  .swiper-button-prev {
+    width: 26px;
+    height: 26px;
+  }
+}
+
+.swiper-button-next::after,
+.swiper-button-prev::after {
+  color: #bf842b; /* ゴールド */
+}
+</style>
---
+++ app/components/views/top/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <PartsSlider :slides="slides" />
+</template>
+
+<script setup lang="ts">
+import slider_image1 from '~/assets/img/top/slider/slide1.png'
+import slider_image2 from '~/assets/img/top/slider/slide2.png'
+import slider_image3 from '~/assets/img/top/slider/slide3.png'
+import slider_image4 from '~/assets/img/top/slider/slide4.png'
+
+const slides = [
+  {
+    image: slider_image1,
+    title: 'Solving Social Issues Together',
+    description: '共に社会課題を解決する'
+  },
+  {
+    image: slider_image2,
+    title: 'Advancing Quality of Life through Medical Innovation',
+    description: '医療イノベーションで生活の質を向上させる'
+  },
+  {
+    image: slider_image3,
+    title:
+      "Skills and Values-based Education through Japan's Technological Expertise",
+    description: 'Future starts here.'
+  },
+  {
+    image: slider_image4,
+    title: 'Enduring Bonds, Sustainable Growth',
+    description: '揺るぎない絆、持続可能な成長を'
+  }
+]
+</script>
