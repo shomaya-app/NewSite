b93dad1bd5f70c03dd41cc01d51436af219cc595
shomaya-app <mayahara.dev@gmail.com>
2026-02-24 01:25:56 +0900

    #5 Headerのレスポンシブ対応

--- app/components/templates/Header/index.vue
+++ app/components/templates/Header/index.vue
@@ -2,8 +2,10 @@
   <div
     class="mx-auto flex h-[100px] max-w-[1180px] items-center border-b-gray-200 px-[20px]"
   >
-    <h1 class="items-center text-[18px]">
-      一般社団法人 日本バングラデシュコンサルティング＆トレーディング
+    <h1 class="pc:text-[18px] items-center text-[16px]">
+      一般社団法人 日本バングラデシュ<br
+        class="pc:hidden block"
+      />コンサルティング＆トレーディング
     </h1>
   </div>
   <PartsNavigation />
