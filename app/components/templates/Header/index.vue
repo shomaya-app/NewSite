47b653619818aa38da93d70ad2391cc12fa72b03
shomaya-app <mayahara.dev@gmail.com>
2026-02-25 01:24:20 +0900

    #5 英語に変更

--- app/components/templates/Header/index.vue
+++ app/components/templates/Header/index.vue
@@ -3,9 +3,8 @@
     class="mx-auto flex h-[100px] max-w-[1180px] items-center border-b-gray-200 px-[20px]"
   >
     <h1 class="pc:text-[18px] items-center text-[16px]">
-      一般社団法人 日本バングラデシュ<br
-        class="pc:hidden block"
-      />コンサルティング＆トレーディング
+      Association Japan Bangladesh<br class="pc:hidden block" />Consulting &
+      Trading
     </h1>
   </div>
   <PartsNavigation />
